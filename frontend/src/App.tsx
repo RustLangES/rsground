c26d2b67cb779fd42eb67d588609785b50a68b58
Brayan-724 <bjqf0724@gmail.com>
2025-04-04 13:25:45 -0700

    refactor(frontend): rename `core` to `features`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import "./App.sass";
 import "../public/fonts/inter.css"
 
 import { Component } from "solid-js";
-import { Sidebar } from "./core/sidebar";
-import { Panels } from "./core/panels";
-import { interceptAuthCallback } from "./core/auth";
+import { Sidebar } from "./features/sidebar";
+import { Panels } from "./features/panels";
+import { interceptAuthCallback } from "./features/auth";
 
 const App: Component = () => {
   interceptAuthCallback();

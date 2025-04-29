4671e4fe8758bddbc2baae1d135254c65754cdaf
Brayan-724 <bjqf0724@gmail.com>
2025-04-29 15:50:33 -0700

    chore(frontend/editor): untrack dockview

--- frontend/src/features/editor/services/editor.ts
+++ frontend/src/features/editor/services/editor.ts
@@ -1,11 +1,12 @@
+import { untrack } from "solid-js";
 import { dockview } from "@features/panels/stores";
 
 export async function openFile(filepath: string) {
   const id = `file:${filepath}`;
   const filename = filepath.split("/").pop();
 
-  if (!dockview().getPanel(id)) {
-    dockview().addPanel({
+  if (!untrack(dockview).getPanel(id)) {
+    untrack(dockview).addPanel({
       id,
       component: "code",
       title: filename,

0deb9f535f51aee536a74f4c8a0ddb9e5155d5b4
Brayan-724 <bjqf0724@gmail.com>
2025-06-18 02:11:41 -0700

    feat(backend): Add utility for `Arc<str>`

--- backend/src/utils/mod.rs
+++ backend/src/utils/mod.rs
@@ -2,6 +2,16 @@ mod async_from;
 mod expect_var;
 mod to_stream;
 
+use std::sync::{Arc, LazyLock};
+
 pub use async_from::AsyncInto;
 pub use expect_var::expect_var;
 pub use to_stream::ToStream;
+
+/// Use `Arc` instead of `String` because **user's id** is a immutable string
+/// and is shared across threads.
+///
+/// https://blocklisted.github.io/blog/arc_str_vs_string_is_it_really_faster/
+pub type ArcStr = std::sync::Arc<str>;
+
+pub static EMPTY_STR: LazyLock<ArcStr> = LazyLock::new(|| Arc::from(""));

ca456b97b4045ac9e82327ec4e619e8e6935fbd9
Brayan-724 <bjqf0724@gmail.com>
2025-06-19 12:35:36 -0700

    fix(frontend): Add `vite-plugin-wasm`

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 import devtools from "solid-devtools/vite";
+import wasm from "vite-plugin-wasm"
 
 export default defineConfig({
   plugins: [
+    wasm(),
     devtools({ autoname: true }),
     solidPlugin(),
     tsconfigPaths(),
